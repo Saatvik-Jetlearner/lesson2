a0df6c06a7226d80269b1cf376f38715ccab02f7
Saatvik-Jetlearner <saatviky14@gmail.com>
2025-08-24 16:00:32 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
-stuDetails = ("Surabhi", 89)
-address = ('227', 'Brickfield Shelters', 'Bangalore', 'Karnataka', '562107')
-for x in address:
-    print(x, end = ' ')
-houseno, apartName, city, state, pin = address
-print()
-print('HNO:', houseno)
-print('APT NO ', apartName)
-print(city)
-print(state)
-print(pin)
-my_tuple = 3, 4.6, "dog"
-print(my_tuple)
-n_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
-print(n_tuple[0][3])
-print(n_tuple[1][1])
-my_tuple = ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z')
-print(my_tuple[1:4])
-print(my_tuple[:-7])
-print(my_tuple[7:])
-my_tuple = (4, 2, 3, [6, 5])
-my_tuple[3][0] = 9
-print(my_tuple)
-my_tuple = ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z')
-print(my_tuple)
+import pgzrun
+from random import randint
+from time import time
 
+WIDTH = 800
+HEIGHT = 600
 
+satellites = []
+lines = []
+next_satellite = 0
 
+start_time = 0
+total_time = 0
+end_time = 0
+number_of_satellite = 8
+def create_satellites():
+    global start_time
+    for count in range(0, number_of_satellite):
+        satellite = Actor("satellite")
+        satellite.pos = (randint(40, WIDTH-40), randint(40, HEIGHT-40))
+        satellites.append(satellite)
+    start_time = time()
+def draw():
+    global total_time
+    screen.blit("background", (0, 0))
+    for satellite in satellites:
+        screen.draw.text(str(number), (satellite.pos[0], satellite.pos[1]+20))
+        satellite.draw()
+        number = number + 1
+    for line in lines:
+        screen.draw.line(line[0], line[1], (255, 255, 255)) 
+    if next_satellite < len(satellites):
+        total_time = time() - start_time
+        screen.draw.text(str(round(total_time, 1)), (10, 10), fontsize = 30)
+    else:
+        screen.draw.text(str(round(total_time, 1)), (10, 10), fontsize = 30)
+def update():
+    pass
+def on_mouse_down(pos):
+    global next_satellite, lines
+    if next_satellite < number_of_satellite:
+        if satellites[next_satellite].collidepoint(pos):
+            lines.append((satellites[next_satellite-1].pos, satellites[next_satellite].pos))
+        next_satellite = next_satellite + 1
+    else:
+        lines = []
+        next_satellite = 0
+create_satellites()
 
+pgzrun.go()
