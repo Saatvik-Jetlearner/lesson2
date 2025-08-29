75df9641bbc356f95b599d0a4fd00183c971673b
Saatvik-Jetlearner <saatviky14@gmail.com>
2025-08-28 20:19:47 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pgzrun
 from random import randint
 from time import time
 
+
 WIDTH = 800
 HEIGHT = 600
 
 satellites = []
 lines = []
 next_satellite = 0
-
 start_time = 0
 total_time = 0
 end_time = 0
@@ -23,13 +23,14 @@ def create_satellites():
 def draw():
     global total_time
     screen.blit("background", (0, 0))
+    number = 1
     for satellite in satellites:
         screen.draw.text(str(number), (satellite.pos[0], satellite.pos[1]+20))
         satellite.draw()
         number = number + 1
     for line in lines:
         screen.draw.line(line[0], line[1], (255, 255, 255)) 
-    if next_satellite < len(satellites):
+    if next_satellite < number_of_satellite:
         total_time = time() - start_time
         screen.draw.text(str(round(total_time, 1)), (10, 10), fontsize = 30)
     else:
