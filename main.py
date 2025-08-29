c0b4662894d15d94d0ab791eb081a94155033849
Saatvik-Jetlearner <saatviky14@gmail.com>
2025-08-28 20:41:58 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,51 +1,12 @@
-import pgzrun
-from random import randint
-from time import time
-
-
-WIDTH = 800
-HEIGHT = 600
-
-satellites = []
-lines = []
-next_satellite = 0
-start_time = 0
-total_time = 0
-end_time = 0
-number_of_satellite = 8
-def create_satellites():
-    global start_time
-    for count in range(0, number_of_satellite):
-        satellite = Actor("satellite")
-        satellite.pos = (randint(40, WIDTH-40), randint(40, HEIGHT-40))
-        satellites.append(satellite)
-    start_time = time()
-def draw():
-    global total_time
-    screen.blit("background", (0, 0))
-    number = 1
-    for satellite in satellites:
-        screen.draw.text(str(number), (satellite.pos[0], satellite.pos[1]+20))
-        satellite.draw()
-        number = number + 1
-    for line in lines:
-        screen.draw.line(line[0], line[1], (255, 255, 255)) 
-    if next_satellite < number_of_satellite:
-        total_time = time() - start_time
-        screen.draw.text(str(round(total_time, 1)), (10, 10), fontsize = 30)
-    else:
-        screen.draw.text(str(round(total_time, 1)), (10, 10), fontsize = 30)
-def update():
-    pass
-def on_mouse_down(pos):
-    global next_satellite, lines
-    if next_satellite < number_of_satellite:
-        if satellites[next_satellite].collidepoint(pos):
-            lines.append((satellites[next_satellite-1].pos, satellites[next_satellite].pos))
-        next_satellite = next_satellite + 1
-    else:
-        lines = []
-        next_satellite = 0
-create_satellites()
-
-pgzrun.go()
+my_set = {1, 2, 3, 4}
+my_set2=set([3, 4, 5, 6])
+print(my_set)
+print(my_set2)
+s = {1, 2, 2, 3}
+print(s)
+s.add(4)
+print(s)
+s.remove(2)
+s.discard(10)
+s.clear()
+print(s)
