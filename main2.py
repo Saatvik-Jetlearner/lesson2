1a1b27c6be6ddad9250653fcd746fe7f10711496
Saatvik-Jetlearner <saatviky14@gmail.com>
2025-11-16 15:54:51 -0500

    Add files via upload

--- main2.py
+++
@@ -1,9 +0,0 @@
-import pygame
-pygame.init()
-WIDTH = 600
-HEIGHT = 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-pygame.display.set_caption("Square, Circle, Rectangle, Line Demo")
-pygame.draw.rect(screen, (0, 0, 255), (300, 300, 25, 25))
-pygame.display.update()
