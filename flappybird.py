8fd0d5258ae51c83f3f47cfbddc20ce6ee599249
Saatvik-Jetlearner <saatviky14@gmail.com>
2025-12-05 18:59:02 -0500

    Add files via upload

--- flappybird.py
+++ flappybird.py
@@ -13,7 +13,8 @@ pygame.display.set_caption('Flappy Bird')
 
 ground_scroll = 0
 scroll_speed = 4
-
+flying = False
+game_over = False
 bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
 
@@ -23,18 +24,33 @@ class Bird(pygame.sprite.Sprite):
         self.images = []
         self.index = 0
         self.counter = 0
-        for i in range(1, 4):
+        for num in range(1, 4):
             img = pygame.image.load(f'img/bird{num}.png')
             self.images.append(img)
         self.image = self.images[self.index]
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
-
+        self.vel = 0
 
     def update(self):
         
+        if flying == True:
+            self.vel += 0.5
+            if self.vel > 8:
+                self.vel = 8
+            self.rect.y += int(self.vel)
+            
+
+        if game_over == False:
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                self.vel = -10
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+
         self.counter += 1
         flap_cooldown = 5
+        
         if self.counter > flap_cooldown:
             self.counter = 0
             self.index += 1
@@ -44,20 +60,32 @@ class Bird(pygame.sprite.Sprite):
 
 bird_group = pygame.sprite.Group()
 
+flappy = Bird(100, int(screen_height / 2))
+bird_group.add(flappy)
+
 
 run = True
 while run:
     clock.tick(fps)
 
-
+    screen.blit(bg, (0,0))
+    bird_group.draw(screen)
+    bird_group.update()
     screen.blit(ground_img, (ground_scroll, 768))
-    ground_scroll -= scroll_speed
-    if abs(ground_scroll) > 35:
-        ground_scroll = 0
+
+    if flappy.rect.bottom > 768:
+        game_over = True
+        flying = False
+    if game_over == False:
+        ground_scroll -= scroll_speed
+        if abs(ground_scroll) > 35:
+            ground_scroll = 0
 
     for event in pygame.event.get():
         if event.type == QUIT:
             run = False
+        if event.type == MOUSEBUTTONDOWN and flying == False and game_over == False:
+            flying = True
 
     pygame.display.update()
 
