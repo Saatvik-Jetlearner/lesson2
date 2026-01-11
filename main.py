6bfe50d0032d77e3bb9835bd88073e6a1852f63f
Saatvik-Jetlearner <saatviky14@gmail.com>
2026-01-11 15:48:46 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,64 @@
 import cv2
 import numpy as np
 
+image = cv2.imread("dog.png")
 
-image = cv2.imread('dog.png')
-
-if image is None:
-    print("Error: Could not read the image.")
-    exit()
-
-cv2.imshow('Dog Image', image)
-
-cv2.imwrite('saved_image.png', image)
-
-gray_image = cv2.imread('dog.png', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('Gray Dog Image', gray_image)
+cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
-blue, green, red = cv2.split(image)
-zeros = np.zeros_like(blue)
+image = cv2.imread("dog.png")
 
-cv2.imshow('Blue Channel', cv2.merge([blue, zeros, zeros]))
-cv2.imshow('Green Channel', cv2.merge([zeros, green, zeros]))
-cv2.imshow('Red Channel', cv2.merge([zeros, zeros, red]))
+kernel = np.ones((5, 5), np.uint8)
+eroded_image = cv2.erode(image, kernel, iterations=1)
 
-"""
-
-cv2.imread(path) -> Reads an image (default = BGR format)
-
-cv2.imshow(window_name, image) -> Displays an image in a window
-
-cv2.imwrite(filename, image) -> Saves an image to disk
-
-cv2.IMREAD_GRAYSCALE -> Reads the image in grayscale
-
-cv2.split(image) -> Splits the image into Blue, Green, and Red channels
-
-"""
+cv2.imshow('Eroded Image', eroded_image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# image = cv2.imread('dog.png')
+
+# blurred = cv2.blur(image, (5, 5))
+# cv2.imshow('Blurred Image', blurred)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+# image = cv2.imread('dog.png')
+
+# gaussian = cv2.GaussianBlur(image, (5, 5), 0)
+# cv2.imshow('Gaussian Blurred Image', gaussian)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+# image = cv2.imread('dog.png')
+
+# resized = cv2.resize(image, (300, 300))
+# cv2.imshow('Resized Image', resized)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# image = cv2.imread('dog.png')
+
+# border = cv2.copyMakeBorder(
+#     image, 20, 20, 20, 20,
+#     cv2.BORDER_CONSTANT,
+#     value=[0, 0, 255]
+# )
+
+# cv2.imshow('Image with Border', border)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+
+# Erosion -> Reduces white region, removes noise
+# Blurring -> Smoothens the image, reduces detail and noise
+# Resizing -> Changes the dimensions of the image
+# Borders -> Adds a border around the image
