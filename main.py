eb5395c8115d812cf0bed91f6d9bfdae962b35d8
Saatvik-Jetlearner <saatviky14@gmail.com>
2026-01-16 18:34:08 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,161 +1,53 @@
-import pygame
-import os
-pygame.font.init()
-pygame.mixer.init()
+#Refer - https://www.geeksforgeeks.org/opencv-python-tutorial/
 
-WIDTH, HEIGHT = 900, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("First Game!")
-pygame.display.update()
+import cv2
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-YELLOW = (255, 255, 0)
-BORDER = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
+image = cv2.imread('dog.png')
+cv2.imshow('Dog Image', image)
+cv2.waitKey(0)
 
-HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
-WINNER_FONT = pygame.font.SysFont('comicsans', 100)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Gray Dog Image', gray_image)
 
-FPS = 60
-VEL = 5
-BULLET_VEL = 7
-MAX_BULLETS = 3
-HEALTH = 10
-SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
-YELLOW_HIT = pygame.USEREVENT + 1
-RED_HIT = pygame.USEREVENT + 2
-
-YELLOW_SPACESHIP_IMAGE = pygame.image.load(
-    os.path.join('images', 'spaceship_yellow.png'))
-YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
-    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
-
-RED_SPACESHIP_IMAGE = pygame.image.load(
-    os.path.join('images', 'spaceship_red.png'))
-RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
-    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
-
-SPACE = pygame.transform.scale(pygame.image.load(
-    os.path.join('images', 'bg.png')), (WIDTH, HEIGHT))
-
-def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
-    WIN.blit(SPACE, (0, 0))
-    pygame.draw.rect(WIN, BLACK, BORDER)
-
-    red_health_text = HEALTH_FONT.render(
-        "Health: " + str(red_health), 1, WHITE)
-    yellow_health_text = HEALTH_FONT.render(
-        "Health: " + str(yellow_health), 1, WHITE)
-    WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
-    WIN.blit(yellow_health_text, (10, 10))
-
-    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
-    WIN.blit(RED_SPACESHIP, (red.x, red.y))
-
-    for bullet in red_bullets:
-        pygame.draw.rect(WIN, RED, bullet)
-    for bullet in yellow_bullets:
-        pygame.draw.rect(WIN, YELLOW, bullet)
-    pygame.display.update()
-
-def yellow_handle_movement(keys_pressed, yellow):
-    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:
-        yellow.x -= VEL
-    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < 450:
-        yellow.x += VEL
-    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:
-        yellow.y -= VEL
-    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT:
-        yellow.y += VEL
-def red_handle_movement(keys_pressed, red):
-    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH:
-        red.x += VEL
-    if keys_pressed[pygame.K_LEFT] and red.x - VEL > 460:
-        red.x -= VEL
-    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:
-        red.y -= VEL
-    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT:
-        red.y += VEL
-
-def handle_bullets(yellow_bullets, red_bullets, yellow, red):
-    for bullet in yellow_bullets[:]:
-        bullet.x += BULLET_VEL
-        if red.colliderect(bullet):
-            pygame.event.post(pygame.event.Event(RED_HIT))
-            yellow_bullets.remove(bullet)
-        elif bullet.x > WIDTH:
-            yellow_bullets.remove(bullet)
-
-    for bullet in red_bullets[:]:
-        bullet.x -= BULLET_VEL
-        if yellow.colliderect(bullet):
-            pygame.event.post(pygame.event.Event(YELLOW_HIT))
-            red_bullets.remove(bullet)
-        elif bullet.x < 0:
-            red_bullets.remove(bullet)
-
-def draw_winner(text):
-    draw_text = WINNER_FONT.render(text, 1, WHITE)
-    WIN.blit(draw_text, (WIDTH / 2 - draw_text.get_width() /
-                         2, HEIGHT/2 - draw_text.get_height()/2))
-    pygame.display.update()
-    pygame.time.delay(5000)    
-
-def main():
-    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
-    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
-
-    red_bullets = []
-    yellow_bullets = []
-
-    red_health = 10
-    yellow_health = 10
-
-    clock = pygame.time.Clock()
-    run = True
-
-    while run:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LSHIFT and len(yellow_bullets) < MAX_BULLETS:
-                    yellow_bullets.append(pygame.Rect(
-                        yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5))
-                if event.key == pygame.K_RSHIFT and len(red_bullets) < MAX_BULLETS:
-                    red_bullets.append(pygame.Rect(
-                        red.x, red.y + red.height//2 - 2, 10, 5))
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-            elif event.type == RED_HIT:
-                red_health -= 1
-            elif event.type == YELLOW_HIT:
-                yellow_health -= 1
-
-        winner_text = ""
-        if red_health <= 0:
-            winner_text = "Yellow Wins!"
-        
-        if yellow_health <= 0:
-            winner_text = "Red Wins!"
-        
-        if winner_text != "":
-            draw_winner(winner_text)
-            break
-
-        keys_pressed = pygame.key.get_pressed()
-        yellow_handle_movement(keys_pressed, yellow)
-        red_handle_movement(keys_pressed, red)
-        
-        handle_bullets(yellow_bullets, red_bullets, yellow, red)
-
-        draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
-
-    main()
-
-if __name__ == "__main__":
-    main()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
-    
+import cv2
+
+img = cv2.imread('dog.png')
+(row, col) = img.shape[:2]
+
+for i in range(row):
+    for j in range(col):
+        img[i, j] = sum(img[i, j]) * 0.33
+
+cv2.imshow('Grayscale Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+import cv2
+img = cv2.imread("dog.png")
+(rows, cols) = img.shape[:2]
+M = cv2.getRotationMatrix2D((cols / 2, rows / 2), 45, 1)
+res = cv2.warpAffine(img, M, (cols, rows))
+
+cv2.imwrite('result.jpg', res)
+
+import cv2
+img = cv2.imread('dog.png')
+
+edges = cv2.Canny(img, 100, 200)
+
+cv2.imwrite('result.jpg', edges)
+
+
+import cv2
+img = cv2.imread('dog.png')
+
+hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+cv2.imshow('HSV Image', hsvImage)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
