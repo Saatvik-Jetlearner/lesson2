e97553a1069192598209f0594cfae48a7e85d702
Saatvik-Jetlearner <saatviky14@gmail.com>
2026-04-24 18:55:43 -0400

    Add files via upload

---
+++ insertsort.py
@@ -0,0 +1,31 @@
+# arr = [12, 34, 2, 5, 7, 2]
+
+# n = len(arr)
+# for i in range(1, n):
+#         key = arr[i]
+#         print("Key", key)
+#         j = i-1
+#         while j>= 0 and key < arr[j]:
+#             arr[j+1] = arr[j]
+#             j -= 1
+#             print(arr)
+
+#         arr[j+1] = key
+#         print(arr)
+#         print("-"*20)
+
+fruits = ["apple", "kiwi", "banana", "watermelon", "date"]
+
+n = len(fruits)
+for i in range(1, n):
+        key = fruits[i]
+        print("Key", key)
+        j = i-1
+        while j >= 0 and len(key) < len(fruits[j]):
+            fruits[j+1] == fruits[j]
+            j -= 1
+            print(fruits)
+
+        fruits[j+1] == key
+        print(fruits)
+        print("-"*20)
