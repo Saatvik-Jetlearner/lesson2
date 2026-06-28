c92ab03f8be8c0ff15a3aaacccb42ab3fa8f3ebe
Saatvik-Jetlearner <saatviky14@gmail.com>
2026-06-28 15:06:57 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+data = pd.Series([10, 20, 30, 40], name = "Numbers")
+print(data)
 data = pd.read_csv("titanic.csv")
 
 print(data.info())
@@ -7,5 +9,22 @@ print(data.info())
 print(data.head())
 print(data.head(3))
 
-print(data.tail)
+print(data.tail())
 print(data.tail(3))
+
+print(data["Name"])
+
+print(data["Age"].max())
+print(data["Age"].min())
+print(data["Age"].count())
+print(data["Age"].mean())
+print(data["Age"].median())
+
+
+print(data.describe())
+
+
+print(data[["Name", "Pclass", "Age"]])
+print(data[data["Age"] < 18])
+print(data[(data["Pclass"] == 1) & (data["Age"] > 18) ])
+print(data[data["Age"] > 18])
