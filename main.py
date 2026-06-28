3cc539bdc038a7ef35b06b46213956ff27e8e34f
Saatvik-Jetlearner <saatviky14@gmail.com>
2026-06-28 15:59:33 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print(data)
 data = pd.read_csv("titanic.csv")
 
 print(data.info())
-
+print(data.shape)
 print(data.head())
 print(data.head(3))
 
@@ -28,3 +28,12 @@ print(data[["Name", "Pclass", "Age"]])
 print(data[data["Age"] < 18])
 print(data[(data["Pclass"] == 1) & (data["Age"] > 18) ])
 print(data[data["Age"] > 18])
+
+count = data[data["Age"] > 60].shape[0]
+print("Passengers above 60: {}".format(count))
+
+count2 = data[(data["Sex"] == "female") & (data["Pclass"] == 1) ].shape[0]
+print("Female 1st class passengers: {}".format(count2))
+
+avg_fare_third = data[data["Pclass"] == "3"]["Fare"].mean()
+print("Average 3rd class ticket fare: {}".format(avg_fare_third))
