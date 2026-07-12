b3ccca292909b31c2c5d1fe981ef66ab7a0886b6
Saatvik-Jetlearner <saatviky14@gmail.com>
2026-07-12 15:55:05 -0400

    Add files via upload

---
+++ example.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4]
+y = [2, 4, 6, 8]
+
+plt.plot(x, y)
+plt.scatter(x, y)
+
+fruits = ["Apple", "Banana", "Orange"]
+count = [5, 3, 7]
+
+plt.bar(fruits, count)
+
+marks = [60, 70, 75, 80, 85, 90, 95]
+
+plt.hist(marks)
+plt.show()
+plt.show()
